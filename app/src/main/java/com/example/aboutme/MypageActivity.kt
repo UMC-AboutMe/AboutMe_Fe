9809ad7df0befc5680925ee71128dd3f1b04a9cb
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-01-15 18:51:12 +0900

    feat : back button 추가 #9

--- app/src/main/java/com/example/aboutme/MypageActivity.kt
+++ app/src/main/java/com/example/aboutme/MypageActivity.kt
@@ -2,10 +2,20 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aboutme.databinding.ActivityMypageBinding
+import com.example.aboutme.databinding.ActivitySearchProfBinding
 
 class MypageActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMypageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMypageBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_mypage)
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
     }
+
+
 }
