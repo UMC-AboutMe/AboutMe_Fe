ca6f2a97d8f041483d3a57d9241c7dd960c95e08
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-01-17 15:40:39 +0900

    add : mypage_setting 파일 추가 #9

---
+++ app/src/main/java/com/example/aboutme/MyapgeSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyapgeSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myapge_setting)
+    }
+}
