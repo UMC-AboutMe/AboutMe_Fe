8239fda0a93994dd8aa3b0c97410e16fa71caea9
Daemon Jung <jjjssswww1006@gmail.com>
2024-01-20 00:02:53 +0900

    comment: 브랜치 분리에 따른 임시 주석처리

--- app/src/main/java/com/example/aboutme/MySpaceStep1Activity.kt
+++ app/src/main/java/com/example/aboutme/MySpaceStep1Activity.kt
@@ -1,4 +1,21 @@
 package com.example.aboutme
 
-class MySpaceStep1Activity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMySpaceStep1Binding
+
+class MySpaceStep1Activity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMySpaceStep1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_myspacestep1)
+    }
+
 }
