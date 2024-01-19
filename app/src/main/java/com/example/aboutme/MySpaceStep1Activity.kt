6d56c021aa3448c6d579165675571ff67c131f4f
Daemon Jung <jjjssswww1006@gmail.com>
2024-01-20 02:50:00 +0900

    fix: 브랜치 분리에 따른 기존 마이스페이스 액티비티 비활성화

--- app/src/main/java/com/example/aboutme/MySpaceStep1Activity.kt
+++ app/src/main/java/com/example/aboutme/MySpaceStep1Activity.kt
@@ -2,15 +2,12 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.CompoundButton
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.example.aboutme.databinding.ActivityMySpaceStep1Binding
+//import com.example.aboutme.databinding.ActivityMySpaceStep1Binding
 
 class MySpaceStep1Activity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMySpaceStep1Binding
+//    private lateinit var binding: ActivityMySpaceStep1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
