18bbee0182c6af244d1aa4a340f35e7d5dac84c2
Daemon Jung <jjjssswww1006@gmail.com>
2024-01-24 04:21:53 +0900

    add: 아지트 페이지 기초코드 추가 #10

---
+++ app/src/main/java/com/example/aboutme/AgitFragment.kt
@@ -0,0 +1,50 @@
+package com.example.aboutme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.aboutme.databinding.FragmentAgitBinding
+
+class AgitFragment : Fragment() {
+
+    lateinit var binding : FragmentAgitBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentAgitBinding.inflate(inflater,container,false)
+
+        initRecycler()
+
+        return binding.root
+    }
+
+    private fun initRecycler(){
+        val itemList = mutableListOf<AgitSpaceData>()
+
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+        itemList.add(AgitSpaceData(R.drawable.agit_space, "00's 스페이스"))
+
+
+        val rvadapter = AgitSpaceRVAdapter(itemList)
+
+        binding.spaceStorageRv.adapter = rvadapter
+
+        binding.spaceStorageRv.layoutManager = GridLayoutManager(requireContext(), 2)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/aboutme/AgitSpaceData.kt
@@ -0,0 +1,6 @@
+package com.example.aboutme
+
+data class AgitSpaceData(
+    val space_img : Int,
+    val space_name : String
+)
