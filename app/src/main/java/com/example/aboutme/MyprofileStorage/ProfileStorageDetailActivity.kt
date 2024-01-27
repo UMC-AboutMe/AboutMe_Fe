00a27c7c6cc459ca7b31487b8cb8016621faec9f
jeong724 <oliveyou724@gmail.com>
2024-01-28 03:29:11 +0900

    [add] 프로필 보관함 액티비티 추가

---
+++ app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aboutme.MyprofileStorage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aboutme.R
+
+class ProfileStorageDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_storage_detail)
+    }
+}
