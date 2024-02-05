b81675d811dd1a20a37d68c2987664f90dd80efe
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-05 14:39:39 +0900

    add : 보관함 저장 api(수정 필요) #8

--- app/src/main/java/com/example/aboutme/Search/SearchProfActivity.kt
+++ app/src/main/java/com/example/aboutme/Search/SearchProfActivity.kt
@@ -2,8 +2,15 @@ package com.example.aboutme.Search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.example.aboutme.Search.api.SearchItf
+import com.example.aboutme.Search.api.SearchObj
+import com.example.aboutme.Search.api.SearchResponse
 import com.example.aboutme.databinding.ActivitySearchProfBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchProfActivity : AppCompatActivity() {
 
@@ -17,6 +24,9 @@ class SearchProfActivity : AppCompatActivity() {
 
         //Dialog
         binding.addBtn.setOnClickListener {
+            //상대방 마이프로필 내 보관함에 추가하기
+            postProfStorage()
+
             CustomDialog("내 프로필도 공유 하시겠습니까?")
                 .show(supportFragmentManager, "ProfDialog")
         }
@@ -37,6 +47,40 @@ class SearchProfActivity : AppCompatActivity() {
                 binding.exampleIv.visibility = View.GONE
             }
         }
+    }
 
+    //상대방 마이프로필 내 보관함에 추가하기
+    private fun postProfStorage(){
+        Log.d("Retrofit","보관함 추가 실행")
+        val requestStoreProf = SearchResponse.RequestStoreProf(listOf(123456, 987654))
+        val call = SearchObj.getRetrofitService.postProfStorage(1, requestStoreProf)
+
+        call.enqueue(object : Callback<SearchResponse.ResponseStoreProf> {
+            override fun onResponse(
+                call: Call<SearchResponse.ResponseStoreProf>, response: Response<SearchResponse.ResponseStoreProf>
+            ) {
+                Log.d("Retrofit",response.toString())
+                if(response.isSuccessful){
+                    val response=response.body()
+                    Log.d("Retrofit",response.toString())
+
+                    if(response != null) {
+                        if(response.isSuccess){
+                            //성공했을 때
+                        }
+                        else{
+                            //실패했을 때
+                            Log.d("Retrofit",response.message)
+
+                        }
+                    }
+                }
+            }
+            override fun onFailure(call:Call<SearchResponse.ResponseStoreProf>, t:Throwable) {
+                val errorMessage = "Call Failed:  ${t.message}"
+                Log.d("Retrofit",errorMessage)
+            }
+        }
+        )
     }
 }
