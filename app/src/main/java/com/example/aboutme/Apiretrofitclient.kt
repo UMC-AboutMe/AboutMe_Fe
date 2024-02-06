6681ee1dee44d4499d31d245bdead5b80a05daea
Daemon Jung <jjjssswww1006@gmail.com>
2024-02-06 19:23:31 +0900

    feat: 마이스페이스 생성시 서버에서 사용자 데이터 추출 #10

--- app/src/main/java/com/example/aboutme/Apiretrofitclient.kt
+++ app/src/main/java/com/example/aboutme/Apiretrofitclient.kt
@@ -27,3 +27,28 @@ object RetrofitClient {
         retrofit.create(ApitestInterface::class.java)
     }
 }
+
+object RetrofitClient2 {
+    private const val BASE_URL = "http://aboutme-prod-env.eba-3cw2pgyk.ap-northeast-2.elasticbeanstalk.com"
+
+    private const val MEMBER_ID_VALUE = "1"
+
+    val apitest: ApitestInterface by lazy<ApitestInterface> {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+
+        okHttpClientBuilder.addInterceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("member-id", RetrofitClient2.MEMBER_ID_VALUE)
+                .build()
+            chain.proceed(newRequest)
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClientBuilder.build())
+            .build()
+
+        retrofit.create(ApitestInterface::class.java)
+    }
+}
--- app/src/main/java/com/example/aboutme/ApitestData.kt
+++ app/src/main/java/com/example/aboutme/ApitestData.kt
@@ -18,3 +18,22 @@ data class SpaceModel(
     val roomType: Int,
     val favorite: Boolean
 )
+
+data class MySpaceCreateRequest (
+    val nickname: String,
+    val characterType: Int,
+    val roomType: Int
+)
+
+data class MySpaceCreate (
+    val isSuccess: Boolean,
+    val code: String,
+    val message: String,
+    val result: ResultModelmsc
+)
+
+data class ResultModelmsc (
+    val nickname: String,
+    val characterType: Int,
+    val roomType: Int
+)
