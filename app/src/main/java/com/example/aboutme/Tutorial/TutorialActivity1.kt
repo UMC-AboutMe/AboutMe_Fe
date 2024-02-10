3956d5350a2d3bf9c278c4e81f783ce25f1f44f1
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-10 22:29:54 +0900

    remove : 튜토리얼 2,3 삭제

--- app/src/main/java/com/example/aboutme/Tutorial/TutorialActivity1.kt
+++ app/src/main/java/com/example/aboutme/Tutorial/TutorialActivity1.kt
@@ -10,19 +10,19 @@ import com.example.aboutme.databinding.ActivityTutorial1Binding
 
 class TutorialActivity1 : AppCompatActivity() {
 
-    private lateinit var binding : ActivityTutorial1Binding
+    //private lateinit var binding : ActivityTutorial1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTutorial1Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-        //Dialog
-        CustomDialogAlarm("알림을 보내도록 허용하시겠습니까?")
-            .show(supportFragmentManager, "AlarmDialog")
-        //binding= DataBindingUtil.setContentView(this, R.layout.activity_tutorial1)
-        binding.nextBtn1.setOnClickListener {
-            val intent= Intent(this, TutorialActivity2::class.java)
-            startActivity(intent)
-        }
+//        binding = ActivityTutorial1Binding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        //Dialog
+//        CustomDialogAlarm("알림을 보내도록 허용하시겠습니까?")
+//            .show(supportFragmentManager, "AlarmDialog")
+//        //binding= DataBindingUtil.setContentView(this, R.layout.activity_tutorial1)
+//        binding.nextBtn1.setOnClickListener {
+//            val intent= Intent(this, TutorialActivity2::class.java)
+//            startActivity(intent)
+//        }
     }
 }
