0a5c1c5a045f66c44e3e5fde8d1ccee83d918561
Daemon Jung <jjjssswww1006@gmail.com>
2024-02-10 14:46:00 +0900

    add: agit member space 화면에 api 연결 #35

---
+++ app/src/main/java/com/example/aboutme/Agit/AgitMemberSpaceActivity.kt
@@ -0,0 +1,73 @@
+package com.example.aboutme.Agit
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aboutme.R
+import com.example.aboutme.databinding.ActivityAgitMemberSpaceBinding
+
+class AgitMemberSpaceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAgitMemberSpaceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAgitMemberSpaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spaceNameTV: TextView = binding.myspaceTitle
+
+        // AgitSpaceRVAdapter에서 전송한 intent를 전달받는다
+        val spaceName = intent.getStringExtra("spaceNameAgit")
+        val characterType = intent.getIntExtra("characterType", -1)
+        val roomType = intent.getIntExtra("roomType", -1)
+
+        Log.d("YourActivity", "Received spaceName: $spaceName")
+        Log.d("YourActivity", "Received characterType: $characterType")
+        Log.d("YourActivity", "Received roomType: $roomType")
+        Log.d("YourActivity", "Received roomType: $intent")
+
+        val index = spaceName?.indexOf("'s")
+        if (index != -1) {
+            val trimmedSpaceName = index?.let { spaceName.substring(0, it + 2) }
+            spaceNameTV.text = trimmedSpaceName
+        } else {
+            spaceNameTV.text = spaceName
+        }
+
+        // 각종 리소스 설정
+        val imageCharacterId = when(characterType) {
+            1 -> R.drawable.step2_avatar_1
+            2 -> R.drawable.step2_avatar_2
+            3 -> R.drawable.step2_avatar_3
+            4 -> R.drawable.step2_avatar_4
+            5 -> R.drawable.step2_avatar_5
+            6 -> R.drawable.step2_avatar_6
+            7 -> R.drawable.step2_avatar_7
+            8 -> R.drawable.step2_avatar_8
+            9 -> R.drawable.step2_avatar_9
+            else -> {R.drawable.step2_avatar_1}
+        }
+        binding.step3SelectedAvatar.setImageResource(imageCharacterId)
+
+        val imageRoomId = when(roomType) {
+            1 -> R.drawable.step3_room_1
+            2 -> R.drawable.step3_room_2
+            3 -> R.drawable.step3_room_3
+            4 -> R.drawable.step3_room_4
+            else -> {R.drawable.step3_room_1}
+        }
+        binding.step3SelectedRoom.setImageResource(imageRoomId)
+
+        // 화면 상단의 로고 클릭시 화면 종료
+        binding.logo.setOnClickListener {
+            finish()
+        }
+
+        binding.agitMemberspaceTrash.setOnClickListener {
+
+        }
+
+    }
+}
