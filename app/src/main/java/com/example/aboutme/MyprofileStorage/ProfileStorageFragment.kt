c719a63c8af495209dfa668ed9e8240cea12cb3a
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-12 05:11:19 +0900

    feat : 프로필 보관함 새로고침 #30

--- app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageFragment.kt
+++ app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageFragment.kt
@@ -6,12 +6,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.aboutme.MyprofileStorage.api.ProfStorageObj
 import com.example.aboutme.MyprofileStorage.api.ProfStorageResponse
 import com.example.aboutme.R
 import com.example.aboutme.databinding.FragmentProfilestorageBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,6 +27,7 @@ class ProfileStorageFragment : Fragment() {
     lateinit var binding: FragmentProfilestorageBinding
     private val itemList = mutableListOf<ProfileData>()
     private lateinit var rvAdapter: ProfileRVAdapter
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +35,46 @@ class ProfileStorageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfilestorageBinding.inflate(inflater, container, false)
+
+        // 스와이프 리프레쉬 레이아웃 초기화
+        swipeRefreshLayout = binding.swipeRefreshLayout
+        swipeRefreshLayout.setColorSchemeColors(
+            // 스와이프 리프레쉬 레이아웃 색깔 변경 -> 블랙, 화이트
+            ContextCompat.getColor(requireContext(), R.color.black),
+            ContextCompat.getColor(requireContext(), R.color.white)
+        )
+
+        // 스와이프 리프레쉬 동작
+        swipeRefreshLayout.setOnRefreshListener {
+            // Coroutine을 사용하여 지연 작업 수행(UI 응답없음 방지를 위한 순차적 실행)
+            CoroutineScope(Dispatchers.Main).launch {
+                isLoading(true)
+                getProfiles()
+                delay(1500)
+                isLoading(false)
+                swipeRefreshLayout.isRefreshing = false // 새로고침 완료 시 리프레시 아이콘 감춤
+            }
+        }
+
+        // 초기 화면은 항상 최신화 상태 유지
+        CoroutineScope(Dispatchers.Main).launch {
+            getProfiles()
+        }
+
         return binding.root
+    }
+    // 새로고침 상태 여부에 따른 shimmer effect 나타내기
+    private fun isLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.shimmerLayout.startShimmer()
+            binding.shimmerLayout.visibility = View.VISIBLE
+            binding.profileStorageRv.visibility = View.GONE
+        }
+        else {
+            binding.shimmerLayout.stopShimmer()
+            binding.shimmerLayout.visibility = View.GONE
+            binding.profileStorageRv.visibility = View.VISIBLE
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,7 +84,7 @@ class ProfileStorageFragment : Fragment() {
         //val intent = Intent(requireContext(), ProfileStorageDetailFragment::class.java)
 
         //프로필 보관함 조회 api
-        getProfiles()
+        //getProfiles()
 
         binding.searchBtn.setOnClickListener {
             //getSearchProfiles()
