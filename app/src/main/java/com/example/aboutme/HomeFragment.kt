733cc0bad5b5e6a5cbb4079863f1437324c0e9d7
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-12 04:11:39 +0900

    feat : 멤버 소개 뷰 자동 스크롤 추가 #40

--- app/src/main/java/com/example/aboutme/HomeFragment.kt
+++ app/src/main/java/com/example/aboutme/HomeFragment.kt
@@ -2,15 +2,22 @@ package com.example.aboutme
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.LinearSmoothScroller.SNAP_TO_START
 import com.example.aboutme.Alarm.AlarmActivity
 import com.example.aboutme.Mypage.MypageActivity
 import com.example.aboutme.Search.SearchProfActivity
 import com.example.aboutme.Search.SearchSpaceActivity
 import com.example.aboutme.databinding.FragmentHomeBinding
+import java.util.Timer
+import java.util.TimerTask
 
 class HomeFragment : Fragment() {
     lateinit var binding : FragmentHomeBinding
@@ -43,6 +50,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initRecycler()
+        startAutoScroll()
+
     }
 
     private fun initRecycler() {
@@ -50,15 +59,44 @@ class HomeFragment : Fragment() {
         binding.homeitemRc.adapter = HomeAdapter
 
         datas.apply {
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
-            add(HomeFragmentData(R.drawable.home_tut2, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc1, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc2, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc3, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc4, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc5, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc6, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc7, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc8, "Member"))
+            add(HomeFragmentData(R.drawable.home_rc9, "Member"))
 
             HomeAdapter.datas = datas
             HomeAdapter.notifyDataSetChanged()
         }
+    }
+
+    //리싸이클러뷰 자동 스크롤
+    private fun startAutoScroll() {
+        var currentPosition = 0
+        val timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                activity?.runOnUiThread {
+                    val nextPosition = (currentPosition + 1) % datas.size
+                    val smoothScroller = object : LinearSmoothScroller(activity) {
+                        override fun getVerticalSnapPreference(): Int {
+                            return SNAP_TO_START
+                        }
+                    }
+                    smoothScroller.targetPosition = nextPosition
+                    binding.homeitemRc.layoutManager?.startSmoothScroll(smoothScroller)
+                    currentPosition = nextPosition
+                }
+            }
+        }, DELAY_MS, PERIOD_MS)
+    }
+
+    companion object {
+        private const val DELAY_MS = 0L // 시작 딜레이
+        private const val PERIOD_MS = 2000L // 스크롤 간격
     }
 }
