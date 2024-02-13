93598f0e5fbb7fda68c5699bb95406cc3ea3fc0a
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-14 04:31:40 +0900

    fix : 프로필 보관함 상세 정보 -front 완료 #30

--- app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageBackFragment.kt
+++ app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageBackFragment.kt
@@ -7,9 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.aboutme.Myprofile.FrontProfileFragment
+import com.example.aboutme.MyprofileStorage.api.ProfStorageObj
+import com.example.aboutme.MyprofileStorage.api.ProfStorageResponse
 import com.example.aboutme.R
 import com.example.aboutme.databinding.FragmentBackprofileBinding
 import com.example.aboutme.databinding.FragmentProfileStorageBackBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileStorageBackFragment : Fragment() {
 
@@ -18,9 +23,8 @@ class ProfileStorageBackFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val profileId: Long = arguments?.getLong("profId") ?: -1
         Log.d("ProfileStorageDetail", "Back Received profId: $profileId")
-
+        getProfile(profileId)
         binding = FragmentProfileStorageBackBinding.inflate(inflater, container, false)
-
         binding.turnBtn2.setOnClickListener {
             val ft = parentFragmentManager.beginTransaction()
             val fragment = ProfileStorageFrontFragment()
@@ -29,7 +33,50 @@ class ProfileStorageBackFragment : Fragment() {
             fragment.arguments = bundle
             ft.replace(R.id.profileStorage_frame, fragment).commit()
         }
-
         return binding.root
+    }
+    //마이프로필 조회 - 단건
+    private fun getProfile(profId : Long ) {
+        val call = ProfStorageObj.getRetrofitService.getProfList(profId)
+
+        call.enqueue(object : Callback<ProfStorageResponse.ResponseProf> {
+            override fun onResponse(
+                call: Call<ProfStorageResponse.ResponseProf>,
+                response: Response<ProfStorageResponse.ResponseProf>
+            ) {
+                if (response.isSuccessful) {
+                    val response = response.body()
+                    if (response != null) {
+                        if (response.isSuccess) {
+                            // 성공했을 때
+                            Log.d("Retrofit_Get_Success", response.toString())
+                            //새로 추가
+                            response.result.front_features.forEach { feature ->
+                                if (feature.key == "name") {
+                                    //작업하던 부분
+                                    //binding.p.text = feature.value
+                                    Log.d("Retrofit_Get_Success", feature.value.toString())
+                                    return@forEach // 해당 조건을 만족하는 요소를 찾았으므로 반복문을 빠져나갑니다.
+                                } else if (feature.key == null) {
+
+                                }
+                            }
+                        } else {
+                            // 실패했을 때
+                            Log.d("Retrofit_Get_Failed", response.toString())
+                        }
+                    }
+                } else {
+                    Log.d("Retrofit_Get_Failed", response.toString())
+                }
+            }
+            override fun onFailure(
+                call: Call<ProfStorageResponse.ResponseProf>,
+                t: Throwable
+            ) {
+                val errorMessage = "Call Failed:  ${t.message}"
+                Log.d("Retrofit_Get_Error", errorMessage)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageFrontFragment.kt
+++ app/src/main/java/com/example/aboutme/MyprofileStorage/ProfileStorageFrontFragment.kt
@@ -27,9 +27,8 @@ class ProfileStorageFrontFragment : Fragment(){
     ): View? {
         val profileId: Long = arguments?.getLong("profId") ?: -1
         Log.d("ProfileStorageDetail", "Front Received profId: $profileId")
-
+        getProfile(profileId)
         binding = FragmentProfileStorageFrontBinding.inflate(inflater, container, false)
-
         binding.turnBtn.setOnClickListener {
             val ft = parentFragmentManager.beginTransaction()
             val fragment = ProfileStorageBackFragment()
@@ -37,10 +36,66 @@ class ProfileStorageFrontFragment : Fragment(){
             bundle.putLong("profId", profileId)
             fragment.arguments = bundle
             ft.replace(R.id.profileStorage_frame, fragment).commit()
-
         }
-
         return binding.root
     }
+    //마이프로필 조회 - 단건
+    private fun getProfile(profId : Long ) {
+        val call = ProfStorageObj.getRetrofitService.getProfList(profId)
 
+        call.enqueue(object : Callback<ProfStorageResponse.ResponseProf> {
+            override fun onResponse(
+                call: Call<ProfStorageResponse.ResponseProf>,
+                response: Response<ProfStorageResponse.ResponseProf>
+            ) {
+                if (response.isSuccessful) {
+                    val response = response.body()
+                    if (response != null) {
+                        if (response.isSuccess) {
+                            // 성공했을 때
+                            Log.d("Retrofit_Get_Success", response.toString())
+                            val image = response.result.profile_image
+                            val imageResId = when {
+                                image.type == "CHARACTER" && image.charcterType in 1..8 -> {
+                                    when (image.charcterType) {
+                                        1 -> R.drawable.prof_avater1
+                                        2 -> R.drawable.prof_avater2
+                                        3 -> R.drawable.prof_avater3
+                                        4 -> R.drawable.prof_avater4
+                                        5 -> R.drawable.prof_avater5
+                                        6 -> R.drawable.prof_avater6
+                                        7 -> R.drawable.prof_avater7
+                                        8 -> R.drawable.prof_avater8
+                                        else -> R.drawable.prof_avater9
+                                    }
+                                }
+                                image.type == "USER_IMAGE" -> R.drawable.prof_avater1
+                                else -> R.drawable.avatar_basic
+                            }
+                            response.result.front_features.forEach { feature ->
+                                if (feature.key == "name") {
+                                    binding.profileNameEt.text = feature.value
+                                } else if (feature.key == null) {
+                                    binding.profileNumEt.text = feature.value ?: ""
+                                }
+                            }
+                            binding.profileIv.setImageResource(imageResId)
+                            }
+                        } else {
+                            // 실패했을 때
+                            Log.d("Retrofit_Get_Failed", response.toString())
+                        }
+                } else {
+                    Log.d("Retrofit_Get_Failed", response.toString())
+                }
+            }
+            override fun onFailure(
+                call: Call<ProfStorageResponse.ResponseProf>,
+                t: Throwable
+            ) {
+                val errorMessage = "Call Failed:  ${t.message}"
+                Log.d("Retrofit_Get_Error", errorMessage)
+            }
+        })
+    }
 }
