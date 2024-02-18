f57b0157889ecdcaf729d3e1171d837d79488064
hyeumm <118244028+hyeumm@users.noreply.github.com>
2024-02-19 03:45:42 +0900

    fix : 프로필 공유하기 이미지 url

--- app/src/main/java/com/example/aboutme/Search/DialogProfData.kt
+++ app/src/main/java/com/example/aboutme/Search/DialogProfData.kt
@@ -1,7 +1,7 @@
 package com.example.aboutme.Search
 
 class DialogProfData (
-    val profile_img : Int,
+    val profile_img : String,
     val profile_name : String,
     val profile_num : String,
     val serial_number :Int,
